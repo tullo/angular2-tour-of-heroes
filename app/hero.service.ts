999370164be6fb08108aea279bdbcadb773d426e
Andreas Amstutz <andreasamstutz@redpill-linpro.com>
2016-05-26 13:12:56 +0200

    Update to RC1 using @angular/router-deprecated

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from 'angular2/core';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
@@ -9,15 +8,16 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
 
+  // See the "Take it slow" appendix
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve =>
-      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+        setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
     );
   }
 
   getHero(id: number) {
     return Promise.resolve(HEROES).then(
-      heroes => heroes.filter(hero => hero.id === id)[0]
+        heroes => heroes.filter(hero => hero.id === id)[0]
     );
   }
 }
